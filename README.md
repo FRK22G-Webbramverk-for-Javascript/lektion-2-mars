66d8edf00f2dc2bb7f40e1aaa16e8d9e4645a88b
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-03-02 10:31:41 +0100

    Added code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# lektion-2-mars
+# Lektion 2 mars
+
+## Övningar
+
+Shopping cart: https://gist.github.com/zocom-christoffer-wallenberg/147c0782b5b069cf60ba0a2babbab372
---
+++ props-example/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import TodoItem from './components/TodoItem/TodoItem'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ul className='todos'>
+        <TodoItem todo="Köp kaffe" done={true} />
+        <TodoItem todo="Köp kaka" done={false} />
+        <TodoItem todo="Brygg kaffe" done={false} />
+        <TodoItem todo="Drick kaffe" done={false} />
+      </ul>
+    </div>
+  )
+}
+
+export default App
